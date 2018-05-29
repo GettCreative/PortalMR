13d5ca146412ab79f3559dbaad242c5868f937c0
MohammadAlRifai <mohammad94sar@gmail.com>
2018-05-29 17:37:01 +0300

    Add react-native tests

---
+++ App.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import App from './App';
+import 'react-native';
+import Login from './app/components/Login.js';
+import Signup from './app/components/Signup.js';
+import Gallery from './app/components/Gallery.js';
+import renderer from 'react-test-renderer';
+//to run the jest test type: npm test 
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('Login renders correctly', () => {
+  const tree = renderer.create(
+    <Login />
+    ).toJSON();
+  expect(tree).toBeTruthy();
+});
+
+it('Signup renders correctly', () => {
+  const tree = renderer.create(
+    <Signup />
+    ).toJSON();
+  expect(tree).toBeTruthy();
+});
+
+it('Gallery renders correctly', () => {
+  const tree = renderer.create(
+    <Gallery />
+    ).toJSON();
+  expect(tree).toBeTruthy();
+});
