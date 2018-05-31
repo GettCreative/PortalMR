5215cb862b1cec8fafad1714cd79212c655e48b0
Nader Nasr <nadernasr7@gmail.com>
2018-05-31 15:37:38 +0300

    mlab mongoAuth

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://root:abc123@ds141720.mlab.com:41720/portal');
-
+mongoose.connect('mongodb://root:abc123@ds141720.mlab.com:41720/portal?authMechanism=MONGODB-CR');
+// mongoose.connect('mongodb://${user}:${pass}@${uri}/${db}?authMechanism=MONGODB-CR')
 var db = mongoose.connection;
 
 db.on('error', function() {
